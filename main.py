028f67470686662df31e12604c053d3e8655ca93
kevinelfert <elferkevin@gmail.com>
2022-01-29 19:58:18 -0600

    Pushing new main.py using pygame's mixer

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 from datetime import datetime
 # needed for sleep function
 from time import sleep
-#needed to play sound
-from playsound import playsound
+# needed to play sound
+from pygame import mixer
 # needed to send process to Alarm
 import multiprocessing
 # from multiprocessing import Process
 # needed to instantiate alarm
 # from alarm import Alarm
 
-
+mixer.init()
 # alarm = Alarm()
 
 x = 0 
@@ -20,18 +20,17 @@ while x==0:
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
-    alarm_time = "19:38:00"
+    alarm_time = "19:56:30"
+
     if current_time == alarm_time:
         print("alarm on, playing sound")
-        # playsound('Alarm.mp3')
-        p = multiprocessing.Process(target=playsound, args=("Alarm.wav",))
-        p.start()
-        # # r = alarm.start_alarm()
-        # # if(r == 1):
-        # #     p.terminate()
-        # #     playsound(None)
-        # #     x = 1
-        input("press ENTER to stop playback")
-        p.terminate()
+        mixer.music.load('Alarm.mp3')
+        mixer.music.play()
+        r = input("press ENTER to stop playback")
+        if(r==''):
+            mixer.music.stop()
+            x = 1
+        
+        
         
 
